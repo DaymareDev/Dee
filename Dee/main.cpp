affed445cf709face246d377b25e9ee8e1ed67fc
DaymareDev <mariusbrendmoe@hotmail.com>
2014-04-24 13:07:34 +0200

    Working DirectX11 initialization for windows 8

--- Dee/main.cpp
+++ Dee/main.cpp
@@ -1,5 +1,20 @@
 #include <Windows.h>
 #include <windowsx.h>
+#include <d3d11.h>
+#include <DirectXMath.h>
+#include <DirectXPackedVector.h>
+
+
+// global declarations
+IDXGISwapChain *swapchain;             // the pointer to the swap chain interface
+ID3D11Device *dev;                     // the pointer to our Direct3D device interface
+ID3D11DeviceContext *devcon;           // the pointer to our Direct3D device context
+ID3D11RenderTargetView *backbuffer;
+
+// function prototypes
+void InitD3D(HWND hWnd);     // sets up and initializes Direct3D
+void CleanD3D(void);         // closes Direct3D and releases memory
+void RenderFrame();
 
 LRESULT CALLBACK WindowProc(HWND hWnd,
 	UINT message,
@@ -28,7 +43,7 @@ int WINAPI WinMain(HINSTANCE hInstance,
 	wc.hInstance = hInstance;
 	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
 	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
-	wc.lpszClassName = "WindowClass1";
+	wc.lpszClassName = "DeeCore";
 
 	// register the window class
 	RegisterClassEx(&wc);
@@ -37,8 +52,8 @@ int WINAPI WinMain(HINSTANCE hInstance,
 
 	// create the window and use the result as the handle
 	hWnd = CreateWindowEx(NULL,
-		"WindowClass1",    // name of the window class
-		"Our First Windowed Program",   // title of the window
+		"DeeCore",    // name of the window class
+		"Dee Debug Window",   // title of the window
 		WS_OVERLAPPEDWINDOW,    // window style
 		300,    // x-position of the window
 		300,    // y-position of the window
@@ -49,7 +64,7 @@ int WINAPI WinMain(HINSTANCE hInstance,
 		hInstance,    // application handle
 		NULL);    // used with multiple windows, NULL
 	
-
+	InitD3D(hWnd);
 	// display the window on the screen
 	ShowWindow(hWnd, nCmdShow);
 
@@ -57,18 +72,23 @@ int WINAPI WinMain(HINSTANCE hInstance,
 
 	// this struct holds Windows event messages
 	MSG msg;
+	ZeroMemory(&msg, sizeof(MSG));
 
 	// wait for the next message in the queue, store the result in 'msg'
-	while (GetMessage(&msg, NULL, 0, 0))
+	while (msg.message != WM_QUIT)
 	{
+		RenderFrame();
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			// translate keystroke messages into the right format
+			TranslateMessage(&msg);
 
-		// translate keystroke messages into the right format
-		TranslateMessage(&msg);
-
-		// send the message to the WindowProc function
-		DispatchMessage(&msg);
+			// send the message to the WindowProc function
+			DispatchMessage(&msg);
+		}
 	}
-
+	
+	CleanD3D();
 	// return this part of the WM_QUIT message to Windows
 	return msg.wParam;
 }
@@ -93,6 +113,79 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lPara
 	// Handle any messages the switch statement didn't
 	return DefWindowProc(hWnd, message, wParam, lParam);
 }
+
+// this function initializes and prepares Direct3D for use
+void InitD3D(HWND hWnd)
+{
+	// create a struct to hold information about the swap chain
+	DXGI_SWAP_CHAIN_DESC scd;
+
+	// clear out the struct for use
+	ZeroMemory(&scd, sizeof(DXGI_SWAP_CHAIN_DESC));
+
+	// fill the swap chain description struct
+	scd.BufferCount = 1;                                    // one back buffer
+	scd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;     // use 32-bit color
+	scd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;      // how swap chain is to be used
+	scd.OutputWindow = hWnd;                                // the window to be used
+	scd.SampleDesc.Count = 4;                               // how many multisamples
+	scd.Windowed = TRUE;                                    // windowed/full-screen mode
+
+	// create a device, device context and swap chain using the information in the scd struct
+	D3D11CreateDeviceAndSwapChain(NULL,
+		D3D_DRIVER_TYPE_HARDWARE,
+		NULL,
+		NULL,
+		NULL,
+		NULL,
+		D3D11_SDK_VERSION,
+		&scd,
+		&swapchain,
+		&dev,
+		NULL,
+		&devcon);
+
+	// get the address of the back buffer
+	ID3D11Texture2D *pBackBuffer;
+	swapchain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&pBackBuffer);
+
+	// use the back buffer address to create the render target
+	dev->CreateRenderTargetView(pBackBuffer, NULL, &backbuffer);
+	pBackBuffer->Release();
+	// set the render target as the back buffer
+	devcon->OMSetRenderTargets(1, &backbuffer, NULL);
+
+	D3D11_VIEWPORT viewport;
+	ZeroMemory(&viewport, sizeof(D3D11_VIEWPORT));
+
+	viewport.TopLeftX = 0;
+	viewport.TopLeftY = 0;
+	viewport.Width = 800;
+	viewport.Height = 600;
+
+	devcon->RSSetViewports(1, &viewport);
+}
+
+void RenderFrame(void)
+{
+	// clear the back buffer to a deep blue
+	devcon->ClearRenderTargetView(backbuffer, reinterpret_cast<float*>(&DirectX::XMFLOAT4(0.0f, 0.2f, 0.4f, 1.0f))); // reinterpret_cast<float*>(&DirectX::PackedVector::XMCOLOR(0.0f, 0.2f, 0.4f, 1.0f)
+
+	// do 3D rendering on the back buffer here
+
+	// switch the back buffer and the front buffer
+	swapchain->Present(0, 0);
+}
+
+void CleanD3D()
+{
+	// close and release all existing COM objects
+	swapchain->Release();
+	backbuffer->Release();
+	dev->Release();
+	devcon->Release();
+}
+
 
 /**
 Multimonitor stuff
